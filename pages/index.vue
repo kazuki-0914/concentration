80211c01204b62ae5d84ac21e7f347790297b710
kazu0914 <kazu0914.dev@gmail.com>
2020-08-27 18:31:41 +0900

    file list

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,75 @@
 <template>
   <div class="container">
-    
+
     <!-- ・スクロールしなくて済む範囲内にトランプ52枚を緑のDivの中にImage52個で描画する -->
     <div v-for="icon in icons" :key="icon" class="field">
       <img :src="icon" class="trump"/>
     </div>
-
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
-
-// const images = require.context('../assets/png/', false, /\.png$/);
+import {TRUMP_MARKS} from '~/store/constants'
+import {TRUMP_NUMBERS} from '~/store/constants'
 
 @Component
 export default class Trump extends Vue {
-  
-  message = 'カードを表示しました'
-  imageurl = require('@/assets/png/c01.png')
   icons = [
+    require('@/assets/png/c01.png'),
+    require('@/assets/png/c02.png'),
+    require('@/assets/png/c03.png'),
+    require('@/assets/png/c04.png'),
+    require('@/assets/png/c05.png'),
+    require('@/assets/png/c06.png'),
+    require('@/assets/png/c07.png'),
+    require('@/assets/png/c08.png'),
+    require('@/assets/png/c09.png'),
+    require('@/assets/png/c10.png'),
+    require('@/assets/png/c11.png'),
+    require('@/assets/png/c12.png'),
+    require('@/assets/png/c13.png'),
+    require('@/assets/png/d01.png'),
+    require('@/assets/png/d02.png'),
+    require('@/assets/png/d03.png'),
+    require('@/assets/png/d04.png'),
+    require('@/assets/png/d05.png'),
+    require('@/assets/png/d06.png'),
+    require('@/assets/png/d07.png'),
+    require('@/assets/png/d08.png'),
+    require('@/assets/png/d09.png'),
+    require('@/assets/png/d10.png'),
+    require('@/assets/png/d11.png'),
+    require('@/assets/png/d12.png'),
+    require('@/assets/png/d13.png'),
     require('@/assets/png/h01.png'),
     require('@/assets/png/h02.png'),
-    require('@/assets/png/h03.png')
+    require('@/assets/png/h03.png'),
+    require('@/assets/png/h04.png'),
+    require('@/assets/png/h05.png'),
+    require('@/assets/png/h06.png'),
+    require('@/assets/png/h07.png'),
+    require('@/assets/png/h08.png'),
+    require('@/assets/png/h09.png'),
+    require('@/assets/png/h10.png'),
+    require('@/assets/png/h11.png'),
+    require('@/assets/png/h12.png'),
+    require('@/assets/png/h13.png'),
+    require('@/assets/png/s01.png'),
+    require('@/assets/png/s02.png'),
+    require('@/assets/png/s03.png'),
+    require('@/assets/png/s04.png'),
+    require('@/assets/png/s05.png'),
+    require('@/assets/png/s06.png'),
+    require('@/assets/png/s07.png'),
+    require('@/assets/png/s08.png'),
+    require('@/assets/png/s09.png'),
+    require('@/assets/png/s10.png'),
+    require('@/assets/png/s11.png'),
+    require('@/assets/png/s12.png'),
+    require('@/assets/png/s13.png')
   ]
-  //カードを表示する 
-  disp() {
-
-    console.log(this.message)
-  }
-
 }
 </script>
 
@@ -43,7 +82,6 @@ export default class Trump extends Vue {
   display: inline-block;
 }
 .trump {
-
   height: 150px;
   width: 100px;
 }
---
+++ store/constants.ts
@@ -0,0 +1,22 @@
+export const TRUMP_MARKS = [
+    { name: 'SPADE', mark: 's'},
+    { name: 'HEART', mark: 'h'},
+    { name: 'DIAMOND', mark: 'd'},
+    { name: 'CLUB', mark: 'c'}
+]
+
+export const TRUMP_NUMBERS = [
+    { name: 'ACE', number: '1'},
+    { name: 'TWO', number: '2'},
+    { name: 'THREE', number: '3'},
+    { name: 'FOUR', number: '4'},
+    { name: 'FIVE', number: '5'},
+    { name: 'SIX', number: '6'},
+    { name: 'SEVEN', number: '7'},
+    { name: 'EIGHT', number: '8'},
+    { name: 'NINE', number: '9'},
+    { name: 'TEN', number: '10'},
+    { name: 'JACK', number: '11'},
+    { name: 'QUEEN', number: '12'},
+    { name: 'KING', number: '13'}    
+]
