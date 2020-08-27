5023e92d215c45ddb64c6cd29dc21fe674f47dba
kazu0914 <kazu0914.dev@gmail.com>
2020-08-27 20:02:53 +0900

    file list

--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,41 @@
     <!-- ・スクロールしなくて済む範囲内にトランプ52枚を緑のDivの中にImage52個で描画する -->
     <div v-for="icon in icons" :key="icon" class="field">
       <img :src="icon" class="trump"/>
+      <!-- <img v-bind:src="require('../assets/' + img)" /> -->
     </div>
+<!-- -->
+    <div v-for="filename in filenameArray" :key="filename">
+      <p>{{ filename }}</p>
+    </div>
+
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import { Vue, Component, Prop } from 'vue-property-decorator'
 import {TRUMP_MARKS} from '~/store/constants'
 import {TRUMP_NUMBERS} from '~/store/constants'
 
 @Component
 export default class Trump extends Vue {
+
+  //カードを表示する 
+  mounted() {
+      msg: 'hello'
+      // let filenameArray = []
+    for (let index = 0; index < TRUMP_MARKS.length; index++) {
+      let filenameNumArray = []
+      for (let num = 0; num < TRUMP_NUMBERS.length; num++) {
+        let imageName = TRUMP_MARKS[index].mark + ( '00' + TRUMP_NUMBERS[num].number ).slice( -2 )
+        console.log(imageName)
+        
+        //filenameArray.push(imageName)        
+      }
+    }
+    console.log("test")
+    
+  }
+
   icons = [
     require('@/assets/png/c01.png'),
     require('@/assets/png/c02.png'),
